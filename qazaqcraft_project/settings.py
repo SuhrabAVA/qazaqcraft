173337c3ccc5251af09ce513aaaac242f895323a
SuhrabAVA <niskastalker@gmail.com>
2025-07-22 14:58:52 +0500

    Первый коммит проекта с PostgreSQL

--- qazaqcraft_project/settings.py
+++ qazaqcraft_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-^w!n&wz5i18_uwctuda^a9b27g10ue)2!*te8qv(yqpim*g+f1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 if os.getenv('RENDER'):
     import dj_database_url
@@ -86,14 +86,16 @@ WSGI_APPLICATION = 'qazaqcraft_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myprojectdb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
