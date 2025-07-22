fda43d34db78197bad27775aa426ddc768e10d4a
SuhrabAVA <niskastalker@gmail.com>
2025-07-22 18:17:23 +0500

    понизил версию gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn qazaqcraft_project.wsgi:application
--- qazaqcraft_project/settings.py
+++ qazaqcraft_project/settings.py
@@ -37,6 +37,17 @@ ALLOWED_HOSTS = ['*']
 
 if os.getenv('RENDER'):
     import dj_database_url
+    
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myprojectdb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
     DATABASES['default'] = dj_database_url.config(default=os.environ['DATABASE_URL'])
 
 
@@ -86,16 +97,7 @@ WSGI_APPLICATION = 'qazaqcraft_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myprojectdb',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
