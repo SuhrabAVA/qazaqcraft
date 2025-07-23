f5d5c8cc4c4b0a520c274a96cd64c2585b2f4ebb
SuhrabAVA <niskastalker@gmail.com>
2025-07-23 11:11:37 +0500

    Добавил статические файлы

---
+++ staticfiles/js/main.js
@@ -0,0 +1,66 @@
+const swiper = new Swiper('.mySwiper', {
+    // общее
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // ключевые точки
+    breakpoints: {
+        // до 768px — 1 слайд
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 16,
+        },
+        // от 769px до 1024px — 2 слайда
+        769: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        // от 1025px — 3 слайда
+        1025: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+    }
+});
+
+const menuToggle = document.getElementById('menuToggle');
+const headerNav = document.getElementById('headerNav');
+menuToggle.onclick = function() {
+    headerNav.classList.toggle('open');
+    document.body.classList.toggle('menu-open');
+};
+// По клику на ссылку закрываем меню (опционально)
+headerNav.querySelectorAll('a').forEach(link => {
+    link.onclick = () => {
+        headerNav.classList.remove('open');
+        document.body.classList.remove('menu-open');
+    };
+});
+// Для раскрытия подменю Каталог по клику
+document.querySelectorAll('.has-dropdown > a').forEach(link => {
+    link.onclick = function(e) {
+        if (window.innerWidth <= 900) {
+            e.preventDefault();
+            this.parentElement.classList.toggle('open');
+        }
+    }
+});
+
+
+document.querySelectorAll('.lightbox').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const imgSrc = this.getAttribute('href');
+        const lightbox = document.getElementById('lightboxModal');
+        const lightboxImg = document.getElementById('lightboxImg');
+        lightboxImg.src = imgSrc;
+        lightbox.classList.add('active');
+    });
+});
+
+function closeLightbox() {
+    document.getElementById('lightboxModal').classList.remove('active');
+    document.getElementById('lightboxImg').src = '';
+}
